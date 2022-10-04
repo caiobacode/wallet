8ab4948a830559c2c7d0ebad88a0f1b08ad574b3
caiobacode <caioodsf@gmail.com>
2022-10-04 16:03:56 -0300

    requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropType from 'prop-types';
 
 class Header extends Component {
+  state = {
+    gastos: 0,
+    cambio: 'BRL',
+  };
+
   render() {
+    const { gastos, cambio } = this.state;
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">{gastos}</p>
+        <p data-testid="header-currency-field">{cambio}</p>
+      </div>
     );
   }
 }
+const mapStateToProps = ({ user }) => ({
+  email: user.email,
+});
 
-export default Header;
+Header.propTypes = {
+  email: PropType.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <div>carteira</div>
+      <div>
+        <h1>Carteira</h1>
+        <Header />
+      </div>
     );
   }
 }
